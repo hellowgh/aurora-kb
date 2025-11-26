43b0733738e16099fa4f935cb524175350cfd519
hellowgh <hellowgh@qq.com>
2025-11-26 22:38:39 +0800

    init

---
+++ src/main/java/com/aurorakb/AuroraKbApplication.java
@@ -0,0 +1,14 @@
+package com.aurorakb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraKbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuroraKbApplication.class, args);
+		System.out.println("423432");
+	}
+
+}
---
+++ src/test/java/com/aurorakb/AuroraKbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurorakb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuroraKbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
